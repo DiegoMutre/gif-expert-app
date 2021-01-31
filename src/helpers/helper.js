ec941c86fedad28bdb393dacf02898179ef0608d
DiegoMutre <tysonmutre10@gmail.com>
2021-01-31 14:11:52 -0500

    feat: Change api_key.

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,7 +1,7 @@
 export const getGif = async category => {
     const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
         category
-    )}&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    )}&limit=10&api_key=BK9Q2R5MFCTG1mL6A1GgOw6dG1XBywTu`;
     const res = await fetch(url);
     const { data } = await res.json();
     const gifs = data.map(img => {
