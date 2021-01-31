2283321655bf59d9b199d8b0b4716da8272ecba9
DiegoMutre <tysonmutre10@gmail.com>
2021-01-31 13:21:34 -0500

    feat: Create state for AddCategory component, use a onChange event.

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,9 +1,22 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const AddCategory = ({ setCategories }) => {
+    // State for input value
+    const [inputValue, setInputValue] = useState("");
+
+    // Get the values from the input form
+    const handleInputChange = e => {
+        setInputValue(e.target.value);
+    };
+
     return (
         <form>
-            <input type="text" />
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
         </form>
     );
 };
