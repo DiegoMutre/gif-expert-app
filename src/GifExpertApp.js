c1ad22e623b4ddee43af83d6b58a68324945e064
DiegoMutre <tysonmutre10@gmail.com>
2021-01-31 13:17:53 -0500

    feat: Create categories state.

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import AddCategory from "./components/AddCategory";
 
 const GifExpertApp = () => {
+    // State for categories
+    const [categories, setCategories] = useState(["One Punch"]);
+
     return (
         <>
             <h1>GifExpertApp</h1>
-            <AddCategory />
+            <AddCategory setCategories={setCategories} />
         </>
     );
 };
