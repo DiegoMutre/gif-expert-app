d7a24cb10c356bbc3076e71a8d799a8321c94a4c
DiegoMutre <tysonmutre10@gmail.com>
2021-01-31 14:21:18 -0500

    feat: End GifGrid component.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,23 @@
 import PropTypes from "prop-types";
+import { useFetchGif } from "../hooks/useFetchGif";
+import GifGridItem from "./GifGridItem";
 
 const GifGrid = ({ category }) => {
+    // Get the images from useFetchGif custom hook
+    const { data: images, loading } = useFetchGif(category);
+
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className="animate__animated animate__fadeIn">{category}</h3>
+            {loading && (
+                <p className="animate__animated animate__flash">Loading...</p>
+            )}
+
+            <div className="card-grid">
+                {images.map(img => (
+                    <GifGridItem key={img.id} title={img.title} url={img.url} />
+                ))}
+            </div>
         </>
     );
 };
