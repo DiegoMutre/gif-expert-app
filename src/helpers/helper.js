bb0ca3dfe7d5198d44eb7eaed98dfe8f0988f554
DiegoMutre <tysonmutre10@gmail.com>
2021-02-01 18:20:53 -0500

    feat: Add environment variables.

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,7 +1,7 @@
 export const getGif = async category => {
     const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
         category
-    )}&limit=10&api_key=BK9Q2R5MFCTG1mL6A1GgOw6dG1XBywTu`;
+    )}&limit=10&api_key=${process.env.REACT_APP_API_KEY}`;
     const res = await fetch(url);
     const { data } = await res.json();
     const gifs = data.map(img => {
